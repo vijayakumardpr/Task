beb7781bcf079b2b8b0e8effb8b3497a42044d93
vijayakumardpr <vijayakumardpr@gmail.com>
2022-01-24 22:15:37 +0530

    commit

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-let div = document.querySelectorAll(".div-id")
+let div = document.getElementsByClassName("div-id")
+let divTwo = document.querySelectorAll(".div-id")
 
-div = [...div]
+// div = [...div]
+
+console.log(div)
+console.log(divTwo)
 
 div.forEach((e) => (e.style.color = "green"))
