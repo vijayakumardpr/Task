d210eb0ecfb0e829e0cc57f5bcbeaa629f778d99
vijayakumardpr <vijayakumardpr@gmail.com>
2022-01-25 18:12:09 +0530

    commit

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-let div = document.getElementsByClassName("div-id")
-let divTwo = document.querySelectorAll(".div-id")
+// let div = document.getElementsByClassName("div-id")
+// let divTwo = document.querySelectorAll(".div-id")
 
-// div = [...div]
+// // div = [...div]
 
-console.log(div)
-console.log(divTwo)
+// console.log(div)
+// console.log(divTwo)
 
-div.forEach((e) => (e.style.color = "green"))
+// div.forEach((e) => (e.style.color = "green"))
+
+// function speak(message, language = "en-US") {
+//   const msg = new SpeechSynthesisUtterance(message)
+//   msg.lang = language
+//   speechSynthesis.speak(msg)
+// }
+
+// speak('May the Force be with you');
+// speak('Ciao, io sono Daniele', 'it-IT');
+
+// const btn = document.querySelector("button")
+
+// const element = document.querySelector("[data-test]")
+
+// element.style.color = "green"
+
+// btn.addEventListener("dblclick", (e) => {
+//   console.log(e.target)
+// })
