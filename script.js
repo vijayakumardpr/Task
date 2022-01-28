951e4ae5d05adbdfc9722d0904d1de92591a7984
vijayakumardpr <vijayakumardpr@gmail.com>
2022-01-28 09:21:10 +0530

    commit

--- script.js
+++ script.js
@@ -1,46 +1,14 @@
-// let div = document.getElementsByClassName("div-id")
-// let divTwo = document.querySelectorAll(".div-id")
+// const element = document.createElement("div")
+// const spanManual = document.createElement("span")
+// element.innerText = "Hello Vijayakumar"
+// element.style.color = "red"
+// element.className = "test-class"
+// element.id = "test-id"
 
-// // div = [...div]
+// document.body.appendChild(element)
 
-// console.log(div)
-// console.log(divTwo)
+// element.appendChild(spanManual)
 
-// div.forEach((e) => (e.style.color = "green"))
+const btn = document.querySelector("button")
 
-// function speak(message, language = "en-US") {
-//   const msg = new SpeechSynthesisUtterance(message)
-//   msg.lang = language
-//   speechSynthesis.speak(msg)
-// }
-
-// speak('May the Force be with you');
-// speak('Ciao, io sono Daniele', 'it-IT');
-
-// const btn = document.querySelector("button")
-
-// const element = document.querySelector("[data-test]")
-
-// element.style.color = "green"
-
-// btn.addEventListener("dblclick", (e) => {
-//   console.log(e.target)
-// })
-
-// const anchorTag = document.querySelector("a")
-
-// anchorTag.addEventListener("click", (e) => {
-//   e.preventDefault()
-//   console.log("clicked")
-// })
-
-// ;(function () {
-//   var message = "Vijay"
-//   console.log(message)
-// })()
-
-// console.log(message)
-
-var name
-
-console.log(name)
+btn.addEventListener("click", () => {})
