e7de9d3c14d80bbf804c75b3e151970d2c5f5803
vijayakumardpr <vijayakumardpr@gmail.com>
2022-01-30 00:08:47 +0530

    commit

--- script.js
+++ script.js
@@ -1,33 +1,14 @@
-/*
-  TODO: 2. Select the elements with the following IDs
-    * modal
-    * open-modal-btn
-    * close-modal-btn
-    * BONUS: overlay
-*/
+function findLongestWordLength(str) {
+  return str.split(" ").map((element) => {
+    return element.split("").length
+  })
+}
 
-const modal = document.querySelector("#modal")
-const openModalBtn = document.querySelector("#open-modal-btn")
-const closeModalBtn = document.querySelector("#close-modal-btn")
-const overlay = document.querySelector("#overlay")
+console.log(
+  findLongestWordLength("The quick brown fox jumped over the lazy dog")
+)
 
-openModalBtn.addEventListener("click", () => {
-  // TODO: 3. Create a click event listener for the open-modal-btn that adds the class "open" to the modal
-  modal.classList.add("open")
-  // BONUS: Also add the class "open" to the overlay
-  overlay.classList.add("open")
-})
+let arr = [1, 2, 3, 4]
+console.log(arr)
 
-closeModalBtn.addEventListener("click", () => {
-  // TODO: 4. Create a click event listener for the close-modal-btn that removes the class "open" from the modal
-  modal.classList.remove("open")
-  // BONUS: Also remove the class "open" from the overlay
-  overlay.classList.remove("open")
-})
-
-// BONUS: Add a click event listener to the overlay that removes the class "open" from the modal and the overlay
-
-overlay.addEventListener("click", () => {
-  modal.classList.remove("open")
-  overlay.classList.remove("open")
-})
+console.log(Math.max(...arr))
