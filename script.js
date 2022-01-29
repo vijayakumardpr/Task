418d7511455c9e8f385b59cdf893401b95968872
vijayakumardpr <vijayakumardpr@gmail.com>
2022-01-29 13:03:31 +0530

    commit

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
-const form = document.querySelector("#new-item-form")
-const input = document.querySelector("#item-input")
-const list = document.querySelector("#list")
-const completedTask = document.querySelector("#remove-list")
+/*
+  TODO: 2. Select the elements with the following IDs
+    * modal
+    * open-modal-btn
+    * close-modal-btn
+    * BONUS: overlay
+*/
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault()
-  const item = document.createElement("div")
-  item.innerText = input.value
-  item.classList.add("list-item")
-  list.appendChild(item)
+const modal = document.querySelector("#modal")
+const openModalBtn = document.querySelector("#open-modal-btn")
+const closeModalBtn = document.querySelector("#close-modal-btn")
+const overlay = document.querySelector("#overlay")
 
-  input.value = ""
+openModalBtn.addEventListener("click", () => {
+  // TODO: 3. Create a click event listener for the open-modal-btn that adds the class "open" to the modal
+  modal.classList.add("open")
+  // BONUS: Also add the class "open" to the overlay
+  overlay.classList.add("open")
+})
 
-  item.addEventListener("click", () => {
-    list.removeChild(item)
-    completedTask.append(item)
-  })
+closeModalBtn.addEventListener("click", () => {
+  // TODO: 4. Create a click event listener for the close-modal-btn that removes the class "open" from the modal
+  modal.classList.remove("open")
+  // BONUS: Also remove the class "open" from the overlay
+  overlay.classList.remove("open")
+})
+
+// BONUS: Add a click event listener to the overlay that removes the class "open" from the modal and the overlay
+
+overlay.addEventListener("click", () => {
+  modal.classList.remove("open")
+  overlay.classList.remove("open")
 })
