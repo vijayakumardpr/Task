ba5e9c85635828b5b57a38fa1a8db83613383ef3
vijayakumardpr <vijayakumardpr@gmail.com>
2022-02-07 07:54:39 +0530

    commit

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-function test(){
-    console.log("Hi 1");
-    
-}
+const buttons = document.querySelectorAll("button")
+
+document.addEventListener(
+  "click",
+  () => {
+    console.log("Clicked Document")
+  },
+  { capture: false }
+)
+
+document.body.addEventListener(
+  "click",
+  () => {
+    console.log("Clicked Body")
+  },
+  { capture: false }
+)
+buttons.forEach((button) => {
+  button.addEventListener(
+    "click",
+    () => {
+      console.log("Clicked Button")
+    },
+    { capture: false }
+  )
+})
