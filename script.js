efb73e7aa6d7749ea1f319971e0207b92004c39b
vijayakumardpr <vijayakumardpr@gmail.com>
2022-02-10 23:40:33 +0530

    commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener("click", (e) => {
   if (!e.target.matches(".expand-button")) return
   const card = e.target.closest(".card")
+
   const cardBody = card.querySelector(".card-body")
   if (e.target.innerText === "Expand") {
     cardBody.classList.add("show")
@@ -9,4 +10,10 @@ document.addEventListener("click", (e) => {
     cardBody.classList.remove("show")
     e.target.innerText = "Expand"
   }
+})
+
+const person = [1, 2, 3, 4, 5, 6]
+
+Object.entries(person).forEach(([key, value], index, array) => {
+  console.debug([key, value], index, array)
 })
