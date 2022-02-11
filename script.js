acaa3a130e99397ba1c2556ddde30c1ddbad94e0
vijayakumardpr <vijayakumardpr@gmail.com>
2022-02-11 12:49:53 +0530

    commit

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
-document.addEventListener("click", (e) => {
-  if (!e.target.matches(".expand-button")) return
-  const card = e.target.closest(".card")
+function sum(...numbers) {
+  return numbers
+}
 
-  const cardBody = card.querySelector(".card-body")
-  if (e.target.innerText === "Expand") {
-    cardBody.classList.add("show")
-    e.target.innerText = "Collapse"
-  } else {
-    cardBody.classList.remove("show")
-    e.target.innerText = "Expand"
-  }
-})
-
-const person = [1, 2, 3, 4, 5, 6]
-
-Object.entries(person).forEach(([key, value], index, array) => {
-  console.debug([key, value], index, array)
-})
+console.log(sum(1, 2, 3, 4, 5, 6, 7))
