2f1de205f7bbd45a3a6e2f1ea8b1ff02a0a3d112
vijayakumardpr <vijayakumardpr@gmail.com>
2022-02-11 22:17:45 +0530

    commit

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-function majorityElement(array) {
-  return array
-}
+const form = document.querySelector("#form")
+const inputs = document.querySelectorAll("input")
+const items = document.querySelector(".items")
 
-console.log(majorityElement([3, 2, 3]))
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  inputs.forEach((input) => {
+    console.log(input.value)
+    input.value = ""
+  })
+})
