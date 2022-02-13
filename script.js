060717f8abb345f49da61fed5c552e5c40cf2b40
vijayakumardpr <vijayakumardpr@gmail.com>
2022-02-13 23:23:45 +0530

    commit

--- script.js
+++ script.js
@@ -8,7 +8,15 @@ form.addEventListener("submit", (e) => {
     const inputValue = input.value
     input.value = ""
     const list = document.createElement("div")
+    const edit = document.createElement("button")
+    const del = document.createElement("button")
+
+    edit.innerText = "Edit"
+    del.innerText = "Delete"
+
     list.innerText = inputValue
     items.appendChild(list)
+    items.appendChild(edit)
+    items.appendChild(del)
   })
 })
