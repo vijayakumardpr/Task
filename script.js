c7fffa7f2dcc684f105bbbf47cf9848de6e0af60
vijayakumardpr <vijayakumardpr@gmail.com>
2022-04-25 23:50:56 +0530

    commit

--- script.js
+++ script.js
@@ -1,127 +1,93 @@
-const washCar = document.getElementById("wash-car")
-const mowLawn = document.getElementById("mow-lawn")
-const pullWeeds = document.getElementById("pull-weeds")
-const invoice = document.getElementById("invoice-genarator")
-const invoiceTotal = document.getElementById("invoice-total")
-const reset = document.getElementById("invoice-footer")
-const template = document.querySelector(".workshop-template")
+let pwd1 = document.querySelector(".pwd1")
+let pwd2 = document.querySelector(".pwd2")
+let pwd3 = document.querySelector(".pwd3")
+let pwd4 = document.querySelector(".pwd4")
+let input = document.getElementById("input")
+let btn = document.querySelector("button")
 
-let sum = 0
-
-const serviceItems = [
-  { service: "Wash Car", cost: 10 },
-  { service: "Mow Lawn", cost: 20 },
-  { service: "Pull Weeds", cost: 30 },
+let randomPasswordCharacter = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "!",
+  "#",
+  "$",
+  "%",
+  "&",
+  "*",
+  "+",
+  ",",
+  "-",
+  "?",
+  "@",
+  "[",
+  "]",
+  1,
+  2,
+  3,
+  4,
+  5,
 ]
 
-washCar.addEventListener("click", () => {
-  const templateClone = template.content.cloneNode(true)
-  const service = templateClone.querySelector(".services")
-  service.textContent = `${serviceItems[0].service}`
-  const cost = templateClone.querySelector(".service-cost")
-  cost.textContent = `$${serviceItems[0].cost}`
-  washCar.disabled = true
-  // invoice.innerHTML += `
-  // <div class="dummy">
-  // <div> ${serviceItems[0].service} <span class="remove">remove<span></div>
-  //  <div> $${serviceItems[0].cost} </div>
-  // </div>
-  //   `
-  // washCar.disabled = true
-  sum = sum + serviceItems[0].cost
-  invoiceTotal.textContent = `$${sum}`
-  const list = templateClone.querySelector(".service-list")
-  const remove = templateClone.querySelector(".remove")
-  remove.addEventListener("click", () => {
-    invoice.removeChild(list)
-    // list.remove()
-    sum = sum - serviceItems[0].cost
-    invoiceTotal.textContent = `$${sum}`
-    washCar.disabled = false
-  })
-  // document.querySelector(".remove").addEventListener("click", () => {
-  //   const dummy = document.querySelector(".dummy")
-  //   console.log(dummy)
-  //   dummy.remove()
-  //   sum = sum - serviceItems[0].cost
-  //   invoiceTotal.textContent = `$${sum}`
-  //   washCar.disabled = false
-  // })
-  invoice.appendChild(templateClone)
+let passwords1 = []
+let passwords2 = []
+let passwords3 = []
+let passwords4 = []
+
+function randomPassword() {
+  let randomNumber = Math.floor(Math.random() * randomPasswordCharacter.length)
+  return randomPasswordCharacter[randomNumber]
+}
+btn.addEventListener("click", (e) => {
+  e.preventDefault()
+  const inputLength = input.value
+  if (inputLength === "") return
+
+  for (let i = 0; i < inputLength; i++) {
+    passwords1.push(randomPassword())
+    pwd1.value = passwords1.join("")
+  }
+
+  for (let i = 0; i < inputLength; i++) {
+    passwords2.push(randomPassword())
+    pwd2.value = passwords2.join("")
+  }
+
+  for (let i = 0; i < inputLength; i++) {
+    passwords3.push(randomPassword())
+    pwd3.value = passwords3.join("")
+  }
+
+  for (let i = 0; i < inputLength; i++) {
+    passwords4.push(randomPassword())
+    pwd4.value = passwords4.join("")
+  }
+
+  passwords1 = []
+  passwords2 = []
+  passwords3 = []
+  passwords4 = []
 })
 
-mowLawn.addEventListener("click", () => {
-  const templateClone = template.content.cloneNode(true)
-  const service = templateClone.querySelector(".services")
-  service.textContent = serviceItems[1].service
-  const cost = templateClone.querySelector(".service-cost")
-  cost.textContent = `$${serviceItems[1].cost}`
+function copy() {
+  pwd1.select()
+  pwd2.select()
+  pwd3.select()
+  pwd4.select()
+  document.execCommand("copy")
+}
 
-  mowLawn.disabled = true
-  //   invoice.innerHTML += `
-  //   <div class='dummy'>
-  //   <div> ${serviceItems[1].service} <span class="remove1">remove<span></div>
-  //   <div> $${serviceItems[1].cost} </div>
-  //   </div>
-  //    `
-  //   mowLawn.disabled = true
-  sum = sum + serviceItems[1].cost
-  invoiceTotal.textContent = `$${sum}`
-  const list = templateClone.querySelector(".service-list")
-  const remove = templateClone.querySelector(".remove")
-  remove.addEventListener("click", () => {
-    invoice.removeChild(list)
-    // list.remove()
-    sum = sum - serviceItems[1].cost
-    invoiceTotal.textContent = `$${sum}`
-    mowLawn.disabled = false
-  })
-  //   document.querySelector(".remove1").addEventListener("click", () => {
-  //     console.log("clicked")
-  //     const dummy1 = document.querySelector(".dummy")
-  //     dummy1.remove()
-  //     sum = sum - serviceItems[1].cost
-  //     invoiceTotal.textContent = `$${sum}`
-  //     washCar.disabled = false
-  //   })
-  invoice.appendChild(templateClone)
+const numbers = [5, 3, 4]
+
+const final = numbers.reduce((acc, cur) => {
+  return acc + cur
 })
 
-pullWeeds.addEventListener("click", () => {
-  const templateClone = template.content.cloneNode(true)
-  const service = templateClone.querySelector(".services")
-  service.textContent = serviceItems[2].service
-  const cost = templateClone.querySelector(".service-cost")
-  cost.textContent = `$${serviceItems[2].cost}`
-  pullWeeds.disabled = true
-  // invoice.innerHTML += `
-  // <div class='dummy'>
-  // <div> ${serviceItems[2].service} <span class="remove2">remove<span></div>
-  // <div> $${serviceItems[2].cost} </div>
-  // </div>
-  // `
-  // pullWeeds.disabled = true
-  sum = sum + serviceItems[2].cost
-  invoiceTotal.textContent = `$${sum}`
-  const list = templateClone.querySelector(".service-list")
-  const remove = templateClone.querySelector(".remove")
-  remove.addEventListener("click", () => {
-    invoice.removeChild(list)
-    // list.remove()
-    sum = sum - serviceItems[2].cost
-    invoiceTotal.textContent = `$${sum}`
-    pullWeeds.disabled = false
-  })
-  // document.querySelector(".remove2").addEventListener("click", () => {
-  //   const dummy2 = document.querySelector(".dummy")
-  //   dummy2.remove()
-  //   sum = sum - serviceItems[2].cost
-  //   invoiceTotal.textContent = `$${sum}`
-  //   washCar.disabled = false
-  // })
-  invoice.appendChild(templateClone)
-})
-
-reset.addEventListener("click", () => {
-  location.reload()
-})
+console.log(final)
